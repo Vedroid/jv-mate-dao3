0fc5029bfd053824db5e9fdf733be35da1e33fe0
Vadim <Vedroidua@gmail.com>
2021-01-11 16:34:46 +0200

    Added db: Storage

---
+++ src/main/java/ua/vedroid/dao3/db/Storage.java
@@ -0,0 +1,33 @@
+package ua.vedroid.dao3.db;
+
+import java.util.ArrayList;
+import java.util.List;
+import ua.vedroid.dao3.model.Car;
+import ua.vedroid.dao3.model.Driver;
+import ua.vedroid.dao3.model.Manufacturer;
+
+public class Storage {
+    public static final List<Car> cars = new ArrayList<>();
+    public static final List<Driver> drivers = new ArrayList<>();
+    public static final List<Manufacturer> manufacturers = new ArrayList<>();
+
+    private static long carId = 0;
+    private static long driverId = 0;
+    private static long manufacturerId = 0;
+
+    public static void addCar(Car car) {
+        car.setId(carId++);
+        cars.add(car);
+    }
+
+    public static void addDriver(Driver driver) {
+        driver.setId(driverId++);
+        drivers.add(driver);
+    }
+
+    public static Manufacturer addManufacturer(Manufacturer manufacturer) {
+        manufacturer.setId(manufacturerId++);
+        manufacturers.add(manufacturer);
+        return manufacturer;
+    }
+}
