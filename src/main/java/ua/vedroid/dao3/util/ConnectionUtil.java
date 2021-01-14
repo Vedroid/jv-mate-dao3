4dd8fd76e0ae5c223b91f2b465be262becb739fb
Vadim <Vedroidua@gmail.com>
2021-01-14 13:08:58 +0200

    Created ConnectionUtil

---
+++ src/main/java/ua/vedroid/dao3/util/ConnectionUtil.java
@@ -0,0 +1,29 @@
+package ua.vedroid.dao3.util;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class ConnectionUtil {
+    private static final String USER = "postgres";
+    private static final String PASSWORD = "Postpass";
+    private static final String IP = "localhost";
+    private static final String PORT = "5432";
+    private static final String DB = "mate";
+
+    private ConnectionUtil() {
+    }
+
+    public static Connection getConnection() {
+        Properties connectionProperties = new Properties();
+        connectionProperties.put("user", USER);
+        connectionProperties.put("password", PASSWORD);
+        String url = "jdbc:postgresql://" + IP + ":" + PORT + "/" + DB + "?serverTimezone=UTC";
+        try {
+            return DriverManager.getConnection(url, connectionProperties);
+        } catch (SQLException e) {
+            throw new RuntimeException("Can`t establish the connection to DB", e);
+        }
+    }
+}
