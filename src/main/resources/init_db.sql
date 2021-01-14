e7548f73319fb75423b4c185dc7d77db9d20c89a
Vadim <Vedroidua@gmail.com>
2021-01-14 14:30:27 +0200

    Updated init_db.sql

--- src/main/resources/init_db.sql
+++ src/main/resources/init_db.sql
@@ -1,11 +1,15 @@
 CREATE DATABASE mate
-    WITH 
+    WITH
     OWNER = postgres
     ENCODING = 'UTF8'
     LC_COLLATE = 'Russian_Ukraine.1251'
     LC_CTYPE = 'Russian_Ukraine.1251'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
+
+ALTER DATABASE mate OWNER TO postgres;
+
+\connect mate
 
 CREATE SCHEMA taxi_service
     AUTHORIZATION postgres;
@@ -14,14 +18,14 @@ GRANT ALL ON SCHEMA taxi_service TO PUBLIC;
 
 GRANT ALL ON SCHEMA taxi_service TO postgres;
 
-CREATE TABLE taxi_service.manufacturers (
-    id bigserial NOT NULL,
-    name character varying NOT NULL,
-    country character varying NOT NULL,
-    is_deleted boolean NOT NULL DEFAULT false,
-    PRIMARY KEY (id)
+CREATE TABLE taxi_service.manufacturers
+(
+    manufacturer_id      bigserial             NOT NULL,
+    manufacturer_name    character varying     NOT NULL,
+    manufacturer_country character varying     NOT NULL,
+    is_deleted           boolean DEFAULT false NOT NULL,
+    PRIMARY KEY (manufacturer_id)
 );
 
 ALTER TABLE taxi_service.manufacturers
-    OWNER to postgres;
-	
+    OWNER TO postgres;
