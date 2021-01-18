921043ee1e2958c9bb1c0887a5e88fd8c0147a5a
Vadim <Vedroidua@gmail.com>
2021-01-18 20:40:22 +0200

    Added GenericDao

--- src/main/java/ua/vedroid/dao3/dao/DriverDao.java
+++ src/main/java/ua/vedroid/dao3/dao/DriverDao.java
@@ -1,17 +1,7 @@
 package ua.vedroid.dao3.dao;
 
-import java.util.List;
-import java.util.Optional;
 import ua.vedroid.dao3.model.Driver;
 
-public interface DriverDao {
-    Driver create(Driver driver);
+public interface DriverDao extends GenericDao<Driver, Long> {
 
-    Optional<Driver> getById(Long driverId);
-
-    List<Driver> getAll();
-
-    Driver update(Driver driver);
-
-    boolean deleteById(Long driverId);
 }
--- src/main/java/ua/vedroid/dao3/dao/ManufacturerDao.java
+++ src/main/java/ua/vedroid/dao3/dao/ManufacturerDao.java
@@ -1,17 +1,6 @@
 package ua.vedroid.dao3.dao;
 
-import java.util.List;
-import java.util.Optional;
 import ua.vedroid.dao3.model.Manufacturer;
 
-public interface ManufacturerDao {
-    Manufacturer create(Manufacturer manufacturer);
-
-    Optional<Manufacturer> getById(Long manufacturerId);
-
-    List<Manufacturer> getAll();
-
-    Manufacturer update(Manufacturer manufacturer);
-
-    boolean deleteById(Long manufacturerId);
+public interface ManufacturerDao extends GenericDao<Manufacturer, Long> {
 }
